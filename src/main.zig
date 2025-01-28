c31f7eb344d1fd29f0936cce7138e7e0c730603a
WurmWillem <84849889+WurmWillem@users.noreply.github.com>
2025-01-28 15:46:15 +0100

    uhhh

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,34 @@
 const std = @import("std");
-const expect = std.testing.expect;
-const eql = std.mem.eql;
-
-const Registers = struct {
-    R0: i16,
-};
 
 pub fn main() !void {
     // get this into a file later on
-    const file_data =
-        "ADD R0, R0, 1";
+    // const file_data =
+    //     "ADD R0, R0, 1";
 
-    for (file_data) |char| {
-        switch (char) {}
+    // parsing is boring
+    // for (file_data) |char| {
+    //     switch (char) {
+    //        'A'
+    //     }
+    // }
+
+    var instructions: [100]Instruction = undefined;
+    const instruction = Instruction{
+        .operation = Operation.Add,
+        .first_3 = 0,
+        .second_3 = 0,
+        .rest = 1,
+    };
+    instructions[0] = instruction;
+
+    // var registers: [7]i16 = [_]i16{0};
+    var registers = std.mem.zeroes([7]i16);
+
+    std.debug.print("R0: {d}\n", .{registers[0]});
+    for (instructions) |inst| {
+        registers[inst.first_3] = registers[inst.second_3] + inst.rest;
     }
+    std.debug.print("R0: {d}\n", .{registers[0]});
 }
 
 const Operation = enum { Add };
